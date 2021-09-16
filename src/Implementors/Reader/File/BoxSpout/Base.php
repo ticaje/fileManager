54274f0b6de29bfead24c607e61f96a10878a15a
Hector Luis Barrientos Margolles <hbarrientos@eshopworld.com>
2021-09-16 15:01:16 +0200

    Fix possible bug on reading data on unset implementor

--- src/Implementors/Reader/File/BoxSpout/Base.php
+++ src/Implementors/Reader/File/BoxSpout/Base.php
@@ -7,10 +7,12 @@ declare(strict_types=1);
 
 namespace Ticaje\FileManager\Implementors\Reader\File\BoxSpout;
 
-use Box\Spout\Reader\Exception\ReaderNotOpenedException;
+use Box\Spout\Common\Exception\UnsupportedTypeException;
 use Box\Spout\Reader\Common\Creator\ReaderFactory;
+use Box\Spout\Reader\Exception\ReaderNotOpenedException;
 use Box\Spout\Reader\ReaderInterface;
 use Box\Spout\Reader\SheetInterface;
+use Exception;
 use Generator;
 use Iterator;
 use Ticaje\FileManager\Infrastructure\Driver\Reader\Interfaces\Gateway\FileGatewayInterface;
@@ -28,27 +30,27 @@ abstract class Base implements FileGatewayInterface
     protected $type;
 
     /**
+     * Base constructor.
+     * @throws Exception
+     */
+    public function __construct()
+    {
+        try {
+            $this->concretion = ReaderFactory::createFromType($this->type);
+        } catch (UnsupportedTypeException $exception) {
+            throw new Exception("Reader Agent Class does not exist");
+        }
+    }
+
+    /**
      * @inheritDoc
      */
     public function fetchData(string $fileName): Iterator
     {
-        $this->concretion = ReaderFactory::createFromType($this->type);
         $this->concretion->open($fileName);
         $sheet = $this->getActiveSheet();
 
         return $this->yieldData($sheet);
-    }
-
-    /**
-     * @param SheetInterface $sheet
-     *
-     * @return Generator
-     */
-    private function yieldData(SheetInterface $sheet)
-    {
-        foreach ($sheet->getRowIterator() as $row) {
-            yield $row;
-        }
     }
 
     /**
@@ -63,6 +65,18 @@ abstract class Base implements FileGatewayInterface
             if ($sheet->isActive()) {
                 return $sheet;
             }
+        }
+    }
+
+    /**
+     * @param SheetInterface $sheet
+     *
+     * @return Generator
+     */
+    private function yieldData(SheetInterface $sheet)
+    {
+        foreach ($sheet->getRowIterator() as $row) {
+            yield $row;
         }
     }
 
